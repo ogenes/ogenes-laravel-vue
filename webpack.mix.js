c4af04de22ecc186461556aeadccfe2c4e10a445
易怀源 <huaiyuan.yi@yourdream.cc>
2022-02-25 11:54:58 +0800

    不加载bootstrap的css

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,7 @@ const path = require('path');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .vue();
 mix.alias({
     '@': path.join(__dirname, 'resources/js')
 });
