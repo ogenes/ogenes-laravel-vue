c8b20683e124398ca58e7087f920a1589bdedb83
易怀源 <huaiyuan.yi@yourdream.cc>
2022-03-09 18:26:22 +0800

    提供默认title

--- resources/js/router/modules/system.js
+++ resources/js/router/modules/system.js
@@ -12,19 +12,28 @@ const systemRouter = {
       path: 'department',
       component: () => import('@/views/system/department'),
       name: 'DepartmentManage',
-      meta: {}
+      meta: {
+        title: '部门管理',
+        icon: 'department',
+      }
     },
     {
       path: 'user',
       component: () => import('@/views/system/user'),
       name: 'UserManage',
-      meta: {}
+      meta: {
+        title: '用户管理',
+        icon: 'user',
+      }
     },
     {
       path: 'menu',
       component: () => import('@/views/system/menu'),
       name: 'MenuManage',
-      meta: {}
+      meta: {
+        title: '菜单管理',
+        icon: 'menu',
+      }
     },
   ]
 };
